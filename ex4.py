53818e1e8bb62673422cb5f44ebc6913c51af595
Dmitrii Evdokimov <ultra.las664@gmail.com>
2025-05-09 21:36:38 +0400

    1-3 пункты задания 4

--- ex4.py
+++ ex4.py
@@ -6,3 +6,6 @@ list_string = ['1234а', 'Teаst', 'а_telegram']
 list_string = list(map(add_pristavka_for_element_in_list, list_string))
 sorted_a_list_string = [value for value in list_string if value[0]== 'а']
 print(sorted_a_list_string)
+
+
+
