07a4928c639316f5df16d755e687c6dce968e2f8
Dmitrii Evdokimov <ultra.las664@gmail.com>
2025-05-10 10:18:52 +0400

    Выполнил пункт 7 задания 4

---
+++ python_base/ex4/ex4_7.py
@@ -0,0 +1,8 @@
+list_something = ['123', 'ffff', '7fjfi', 'fqkhff', 123]
+
+for i, value in enumerate(list_something):
+    if i == 2:
+        print(value + value)
+    else:
+        print(value)
+
