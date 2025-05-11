068323c38f177ee1a3916904a8b1c06c6df04099
Dmitrii Evdokimov <ultra.las664@gmail.com>
2025-05-11 18:28:55 +0400

    Выполнил задание 5

--- python_base/ex5/ex5.py
+++ python_base/ex5/ex5.py
@@ -1,7 +1,7 @@
 class Exercise5:
     def __init__(self):
         self.__var = 'private_parent'
-        self._protected = 'protected_parent' #5.4
+        self._protected = 'protected_parent'  # 5.4
 
 
 class Ex5_5(Exercise5):
@@ -16,3 +16,4 @@ print("5.2: ", vars(exercise_5))
 print("5.3: ", exercise_5._Exercise5__var)
 print("5.4-5.5: ", exercise_5._protected, new_exercise_5._protected)
 print("5.7: ", vars(Ex5_5))
+print("")
