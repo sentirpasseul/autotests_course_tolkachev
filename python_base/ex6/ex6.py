31ff04f8f98716f17e4c8e9494ff1c11701f402d
Dmitrii Evdokimov <ultra.las664@gmail.com>
2025-05-13 12:44:17 +0400

    Выполнил задание 6, кроме пункта 9

---
+++ python_base/ex6/ex6.py
@@ -0,0 +1,56 @@
+from runpy import run_path
+
+
+def ex6_1():
+    return [x for x in range(1, 1001) if x % 7 == 0]
+
+
+def ex6_2():
+    numbers_7 = ex6_1()
+    return [x if x % 3 == 0 else f'{x}{x}' for x in numbers_7]
+
+
+def ex6_3():
+    string_with_spaces = "  hel l o      world   "
+    return list(tuple(string_with_spaces)).count(' ')
+
+
+def ex6_4():
+    string_words = "Yellow Yaks like yelling and yawning and yesturday they yodled while eating yuky yams"
+    string_words = string_words.split()
+    return [word for word in string_words if word[0] == 'Y' or word[0] == 'y']
+
+
+def ex6_5():
+    string1 = "hi, 3.44, 535  "
+    list_indexes = tuple((index, value) for index, value in zip(string1.split(), [x for x in range(1, 4)]))
+    return (list_indexes)
+
+
+def ex6_6():
+    string_words = "In 1984 there were 13 instances of a protest with over 1000 people attending"
+    return list(word for word in tuple(string_words.split()) if word.isdigit())
+
+
+def ex6_7():
+    return ['четное' if x % 2 == 0 else 'нечетное' for x in range(1, 21)]
+
+
+def ex6_8():
+    string_words = "The trickiest part of learning list comprehension for me was really understanding the syntax."
+    return [word for word in string_words.split() if len(word) < 4]
+
+
+def ex6_9():
+    return [x for x in range(1, 51) if x > 1 and [i for i in range(2, int(i**0.5)+1) if x%i == 0]]
+
+
+print("Задание 1: ", ex6_1())
+print("Задание 2: ", ex6_2())
+print("Задание 3: ", ex6_3())
+print("Задание 4: ", ex6_4())
+print("Задание 5: ", ex6_5())
+print("Задание 6: ", ex6_6())
+print("Задание 7: ", ex6_7())
+print("Задание 8: ", ex6_8())
+print("Задание 9: ", ex6_9())
