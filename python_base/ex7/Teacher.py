5eef61ba2613d2490a2c355ee3e57688b4019132
Dmitrii Evdokimov <ultra.las664@gmail.com>
2025-05-17 19:41:07 +0400

    Пофиксил создание экземпляра студент, теперь работает более корректно

---
+++ python_base/ex7/Teacher.py
@@ -0,0 +1,25 @@
+from Student import Student
+
+
+class Teacher:
+    def __init__(self, subject, student, teacher_name: str):
+        self.subject = subject
+        self.student = student
+        self.teacher_name = teacher_name
+        self.teacher_spare_time = 140
+
+    def print_free_time(self):
+        return self.teacher_spare_time
+
+    def teach_student(self):
+        spare_time = self.take_time(self.subject.hours_subject)
+        print(self.print_free_time(), 'часов свободного времени у преподавателя')
+        print(f'Преподаватель {self.teacher_name} обучает {self.subject.name_subject} студента {self.student.student_name}'
+              f'\nПредмет берет на себя {self.subject.hours_subject} часов')
+        print(f'У преподавателя остается {spare_time} часов')
+
+    def print_hours_subject(self):
+        return self.subject_hours
+
+    def take_time(self, hours):
+        return self.teacher_spare_time - hours
---
+++ python_base/ex7/ex7.py
@@ -0,0 +1,18 @@
+from Student import Student
+from Subject import Subject
+from Teacher import Teacher
+
+
+class Schedule:
+    def __init__(self, student_name, teacher_name, subject_name, subject_hours):
+        self.subject = Subject(subject_name, subject_hours)
+        self.student = Student(student_name)
+        self.teacher_geography = Teacher(teacher_name=teacher_name, subject=self.subject, student=self.student)
+
+
+    def take_time(self):
+        self.teacher_geography.teach_student()
+
+
+schedule = Schedule(teacher_name='Андрей Прокопьев', student_name='Евдокимов Дмитрий', subject_name='География',
+                    subject_hours=20).take_time()
